831b6f100497c8575bddbdb279b8cc1f75dbcb9c
Anthony Barragan <anthony.bar.89@gmail.com>
2025-01-27 23:08:12 -0800

    made labels and inputs

--- main.py
+++ main.py
@@ -2,10 +2,32 @@ from tkinter import *
 from tkinter import ttk
 from take_attendance import take_attendance
 
+# Initialize Window
 root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World").grid(column=0, row=0)
-ttk.Button(frm, text="Initialize", command=take_attendance).grid(column=1, row=0)
+root.title = "Power Pal"
+root.geometry("500x400")
+# root.maxsize(500, 400)
+# root.minsize(400,300)
+
+# Create a Frame
+welcome_frame = ttk.Frame(root, padding=10)
+welcome_frame.grid()
+
+# Welcome Text
+welcome_text = Label(welcome_frame, text="Welcome to Power Pal!")
+welcome_text.grid(column=0, row=0, columnspan=12)
+
+# User Name Entry
+username = ttk.Entry(welcome_frame)
+username.grid(column=0, row=1)
+
+# username.pack()
+
+# User Password Entry
+password = ttk.Entry(welcome_frame)
+password.grid(column=6, row=1)
+
+# ttk.Button(frm, text="update", command=root.update).grid(column=1, row=0)
+
 
 root.mainloop()
